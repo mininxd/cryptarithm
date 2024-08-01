1b8e56165455f40191f6dc9261299d442dbc63df
mininxd <mininsna@gmail.com>
2024-08-01 01:45:25 +0000

    sengaja dibuat sulit

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,23 @@
-var query = "AB+BC=EDD"
+function fetchCrypt(event) {
+var q = event;
+var query = q.replaceAll("/",'÷');
 fetch(`v1?q=${query}`).then(res => {return res.json()})
  .then(data => {
-// output.append(varMapping);
-
-
-   let i = 2;
-   for(i; i < data.length; i++) {
-resultWord.innerHTML += `<div class="word"> ${data[i][0]}</div>`
-   resultNum.innerHTML  += `<div class="number"> ${data[i][1]}</div>`
-   }
-//   output.append(JSON.stringify(data))
-
+ var num = data.var_mapping
+ for(let word in num) {
+  resultWord.innerHTML += `<div class="word"> ${word}</div>`
+  resultNum.innerHTML  += `<div class="number"> ${num[word]}</div>`
+  
+  run.classList.remove('is-loading');
+ }
  }).catch(e => {
    output.append(JSON.stringify(e))
+  run.classList.remove('is-loading');
  })
- 
- let varMapping = '{"A": 8, "B": 7, "C": 9, "D": 6, "E": 1}'
-  let map = JSON.parse(varMapping);
- for (let key in map) {
-  outout.append(key + map[key])
 }
+
+run.addEventListener('click', function(e) {
+  e.preventDefault();
+  fetchCrypt(input.value);
+  run.classList.add('is-loading')
+})
